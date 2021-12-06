1f60776ca08591ed34c81765da7bf7583d47405f
Rakesh Kumar  Singh <52422856+rakeshkumar1019@users.noreply.github.com>
2021-12-06 11:22:00 +0530

    Update IndexServlet.java

--- src/main/java/com/yusufsezer/controller/IndexServlet.java
+++ src/main/java/com/yusufsezer/controller/IndexServlet.java
@@ -13,7 +13,7 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         request.setAttribute("viewFile", "index.jsp");
-        request.setAttribute("pageTitle", "Just Another Java JSP App...");
+        request.setAttribute("pageTitle", "Keep a diary, and someday it'll keep you.");
         request.setAttribute("diaryList", Helper.diaryRepository().getAll());
         request.setAttribute("userList", Helper.userRepository().getAll());
         Helper.view(request, response);
