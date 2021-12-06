a59e5ace752863c2ebdac67f240b6a388f0cc5c6
Rakesh Kumar  Singh <52422856+rakeshkumar1019@users.noreply.github.com>
2021-12-06 11:00:24 +0530

    Update Helper.java

--- src/main/java/com/yusufsezer/util/Helper.java
+++ src/main/java/com/yusufsezer/util/Helper.java
@@ -19,7 +19,7 @@ public class Helper {
 
     public static String VIEW_FOLDER = "WEB-INF/view";
     public static String NOT_FOUND = "notfound.jsp";
-    public static String DB_SOURCE = "jdbc:mysql://jspdiary.cgerml78x6pf.ap-south-1.rds.amazonaws.com:3306/jspdiary?user=admin&password=adminuser";
+    public static String DB_SOURCE =  "jdbc:mysql://jspdiary.cgerml78x6pf.ap-south-1.rds.amazonaws.com:3306/jspDiary?useSSL=false&serverTimezone=UTC&user=admin&password=adminuser&charset=UTF-8";
     private static IDatabase DATABASE = null;
 
     public static void view(HttpServletRequest request, HttpServletResponse response)
