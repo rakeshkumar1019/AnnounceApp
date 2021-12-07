06bdc94e9783cb8500b0a9707be248efa34abd24
Rakesh Kumar  Singh <52422856+rakeshkumar1019@users.noreply.github.com>
2021-12-07 18:21:08 +0530

    Update IndexServlet.java

--- src/main/java/com/yusufsezer/controller/IndexServlet.java
+++ src/main/java/com/yusufsezer/controller/IndexServlet.java
@@ -13,7 +13,7 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         request.setAttribute("viewFile", "index.jsp");
-        request.setAttribute("pageTitle", "Announce is an web based communication system for trainings, seminars and trade fairs.");
+        request.setAttribute("pageTitle", "Community Announcement Portal is a web based announcement portal for broadcasting messages.");
         request.setAttribute("diaryList", Helper.diaryRepository().getAll());
         request.setAttribute("userList", Helper.userRepository().getAll());
         Helper.view(request, response);
